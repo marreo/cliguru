ada5f5789df039aa413f8c5eeb776869ecc8f4cc
Martin Lindgren <martinmarklind@hotmail.com>
2016-11-07 20:34:07 +0100

    Removed db connection, testing env var

--- config/env/development.js
+++ config/env/development.js
@@ -5,7 +5,7 @@ var defaultEnvConfig = require('./default');
 module.exports = {
   db: {
     // uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
-    uri: "mongodb://adm:cliguru@ds139197.mlab.com:39197/zs-test",
+    uri: "",
     options: {
       user: '',
       pass: ''
